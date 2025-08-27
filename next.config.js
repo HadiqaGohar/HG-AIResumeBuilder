c42e8082f05e73cf500fc48429845c1a211b6665
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-08-27 23:06:29 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -87,56 +87,6 @@
 
 // // export default nextConfig;
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "lh3.googleusercontent.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "platform-lookaside.fbsbx.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "cv-creator.co.uk",
-//         port: "",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "file.aiquickdraw.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "website.cdn.novoresume.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-
-//   experimental: {
-//     esmExternals: "loose", // ESM packages like @react-pdf/renderer ke liye
-//     turbo: false,        // Agar turbopack issue kare to isko uncomment kar sakti ho
-//   },
-// };
-
-// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -144,36 +94,46 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
+        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "platform-lookaside.fbsbx.com",
+        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "cv-creator.co.uk",
+        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "file.aiquickdraw.com",
+        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "website.cdn.novoresume.com",
+        port: "",
         pathname: "/**",
       },
     ],
   },
+
+  experimental: {
+    esmExternals: "loose", // ESM packages like @react-pdf/renderer ke liye
+    turbo: false,        // Agar turbopack issue kare to isko uncomment kar sakti ho
+  },
 };
 
-export default nextConfig;
-
+module.exports = nextConfig;
