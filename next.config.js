9c21e6b80fcaa4442b3ff716623f7aec113dee53
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-08-27 22:50:38 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,44 +1,94 @@
+// // // import type { NextConfig } from "next";
+
+// // // const nextConfig: NextConfig = {
+  
+// // //   /* config options here */
+// // //   images: {
+// // //     domains: ['cv-creator.co.uk', "file.aiquickdraw.com", "website.cdn.novoresume.com", "lh3.googleusercontent.com"],
+// // //     // If you have other external image domains, add them here too.
+// // //     // For example: domains: ['example.com', 'another-domain.net'],
+// // //   },
+  
+// // // };
+
+// // // export default nextConfig;
+
+
+
+
+
+
+
+
+
+// // // // Disable Turbopack in next.config.js:
+// // // // If Turbopack is enabled by default, disable it in next.config.js:
+// // // // js
+
+// // // // Collapse
+
+// // // // Wrap
+
+// // // // Copy
+// // // // module.exports = {
+// // // //   experimental: {
+// // // //     turbo: false,
+// // // //   },
+// // // // };
+
 // // import type { NextConfig } from "next";
 
 // // const nextConfig: NextConfig = {
-  
-// //   /* config options here */
 // //   images: {
-// //     domains: ['cv-creator.co.uk', "file.aiquickdraw.com", "website.cdn.novoresume.com", "lh3.googleusercontent.com"],
-// //     // If you have other external image domains, add them here too.
-// //     // For example: domains: ['example.com', 'another-domain.net'],
+// //     remotePatterns: [
+// //       {
+// //         protocol: "https",
+// //         hostname: "lh3.googleusercontent.com",
+// //         port: "",
+// //         pathname: "/**",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "avatars.githubusercontent.com",
+// //         port: "",
+// //         pathname: "/**",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "platform-lookaside.fbsbx.com",
+// //         port: "",
+// //         pathname: "/**",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "cv-creator.co.uk",
+// //         port: "",
+// //         pathname: "/**",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "file.aiquickdraw.com",
+// //         port: "",
+// //         pathname: "/**",
+// //       },
+// //       {
+// //         protocol: "https",
+// //         hostname: "website.cdn.novoresume.com",
+// //         port: "",
+// //         pathname: "/**",
+// //       },
+// //     ],
 // //   },
   
+// //   // experimental: {
+// //   //   turbo: false, // Disable Turbopack if causing issues
+// //   // },
 // // };
 
 // // export default nextConfig;
 
-
-
-
-
-
-
-
-
-// // // Disable Turbopack in next.config.js:
-// // // If Turbopack is enabled by default, disable it in next.config.js:
-// // // js
-
-// // // Collapse
-
-// // // Wrap
-
-// // // Copy
-// // // module.exports = {
-// // //   experimental: {
-// // //     turbo: false,
-// // //   },
-// // // };
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
 //   images: {
 //     remotePatterns: [
 //       {
@@ -79,14 +129,14 @@
 //       },
 //     ],
 //   },
-  
-//   // experimental: {
-//   //   turbo: false, // Disable Turbopack if causing issues
-//   // },
+
+//   experimental: {
+//     esmExternals: "loose", // ESM packages like @react-pdf/renderer ke liye
+//     turbo: false,        // Agar turbopack issue kare to isko uncomment kar sakti ho
+//   },
 // };
 
-// export default nextConfig;
-
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -94,46 +144,36 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "platform-lookaside.fbsbx.com",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "cv-creator.co.uk",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "file.aiquickdraw.com",
-        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "website.cdn.novoresume.com",
-        port: "",
         pathname: "/**",
       },
     ],
   },
-
-  experimental: {
-    esmExternals: "loose", // ESM packages like @react-pdf/renderer ke liye
-    turbo: false,        // Agar turbopack issue kare to isko uncomment kar sakti ho
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
