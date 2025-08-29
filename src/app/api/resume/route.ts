694453ccf38fe391a762be66dd4d87c47e5f5db9
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-08-29 13:40:47 +0500

    Update route.ts

--- src/app/api/resume/route.ts
+++ src/app/api/resume/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: NextRequest) {
   try {
     const requestBody = await req.json(); // Get JSON body from frontend (education, skills)
 
-    const FASTAPI_URL = process.env.NEXT_PUBLIC_FASTAPI_URL || 'http://localhost:8000' || 'https://ehmt8mro7sonvp9cs5oblz.streamlit.app/' || 'https://hg-airesumebuilder-backend-production.up.railway.app/';
+    const FASTAPI_URL = process.env.NEXT_PUBLIC_FASTAPI_URL || 'http://localhost:8000' || 'https://hg-airesumebuilder-backend-production.up.railway.app';
 
     console.log(`[Next.js API] Forwarding resume request to FastAPI: ${FASTAPI_URL}/api/resume`);
 
