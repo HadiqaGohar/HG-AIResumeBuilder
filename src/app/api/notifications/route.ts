2651faf9f2691518e21869e1a73501fa99d15056
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-08-31 14:57:27 +0500

    Update route.ts

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -104,7 +104,9 @@
 
 
 import { NextRequest, NextResponse } from 'next/server';
-import clientPromise from '@/lib/mongodb';
+// import clientPromise from '@/lib/mongodb';
+import clientPromise from '../../../../lib/mongodb';
+
 
 export async function POST(request: NextRequest) {
   try {
