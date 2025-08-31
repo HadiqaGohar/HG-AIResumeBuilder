b5f31ed4bce5a6753589dfc7338fb8029745cfa8
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-08-31 15:27:38 +0500

    Update route.ts

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -191,7 +191,8 @@
 // }
 
 import { NextRequest, NextResponse } from 'next/server';
-import clientPromise from '@/lib/mongodb';
+// import clientPromise from '@/lib/mongodb';
+import clientPromise from '../../../../lib/mongodb';
 
 export async function GET() {
   try {
