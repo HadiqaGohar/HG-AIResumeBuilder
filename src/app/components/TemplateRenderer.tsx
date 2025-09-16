f5af1afa5749a79abc0d00d3af71e1f6123a9c47
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2025-09-16 14:46:35 +0500

    Update TemplateRenderer.tsx

--- src/app/components/TemplateRenderer.tsx
+++ src/app/components/TemplateRenderer.tsx
@@ -1,61 +1,9 @@
-// 'use client';
-// import React from 'react';
-// import { ResumeData } from '../../../lib/store';
-// import Template1 from './templates/Template1';
-// import Template2 from './templates/Template2';
-// import Template3 from './templates/Template3';
-
-// interface TemplateRendererProps {
-//   templateId: string;
-//   data: ResumeData;
-//   onDownload?: () => void;
-// }
-
-// const TemplateRenderer: React.FC<TemplateRendererProps> = ({ 
-//   templateId, 
-//   data, 
-//   onDownload 
-// }) => {
-//   const renderTemplate = () => {
-//     const templateProps = {
-//       data: data,
-//       resumeData: data,
-//       isEditable: false,
-//       showEditButton: false,
-//       onEdit: undefined
-//     };
-
-//     switch (templateId) {
-//       case '1':
-//         return <Template1 {...templateProps} />;
-//       case '2':
-//         return <Template2 {...templateProps} onDownload={onDownload} />;
-//       case '3':
-//         return <Template3 {...templateProps} onDownload={onDownload} />;
-//       default:
-//         return <Template1 {...templateProps} />;
-//     }
-//   };
-
-//   return (
-//     <div className="template-renderer w-full h-full">
-//       <div className="w-full h-full overflow-hidden">
-//         {renderTemplate()}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TemplateRenderer;
-
-
-"use client";
-
-import React from "react";
-import { ResumeData } from "../../../lib/store";
-import Template1 from "./templates/Template1";
-import Template2 from "./templates/Template2";
-import Template3 from "./templates/Template3";
+'use client';
+import React from 'react';
+import { ResumeData } from '../../../lib/store';
+import Template1 from './templates/Template1';
+import Template2 from './templates/Template2';
+import Template3 from './templates/Template3';
 
 interface TemplateRendererProps {
   templateId: string;
@@ -63,10 +11,10 @@ interface TemplateRendererProps {
   onDownload?: () => void;
 }
 
-const TemplateRenderer: React.FC<TemplateRendererProps> = ({
-  templateId,
-  data,
-  onDownload,
+const TemplateRenderer: React.FC<TemplateRendererProps> = ({ 
+  templateId, 
+  data, 
+  onDownload 
 }) => {
   const renderTemplate = () => {
     const templateProps = {
@@ -74,15 +22,15 @@ const TemplateRenderer: React.FC<TemplateRendererProps> = ({
       resumeData: data,
       isEditable: false,
       showEditButton: false,
-      onEdit: undefined,
+      onEdit: undefined
     };
 
     switch (templateId) {
-      case "1":
+      case '1':
         return <Template1 {...templateProps} />;
-      case "2":
+      case '2':
         return <Template2 {...templateProps} onDownload={onDownload} />;
-      case "3":
+      case '3':
         return <Template3 {...templateProps} onDownload={onDownload} />;
       default:
         return <Template1 {...templateProps} />;
@@ -90,8 +38,8 @@ const TemplateRenderer: React.FC<TemplateRendererProps> = ({
   };
 
   return (
-    <div className="template-renderer w-full h-auto">
-      <div className="w-full h-auto overflow-auto">
+    <div className="template-renderer w-full h-full">
+      <div className="w-full h-full overflow-hidden">
         {renderTemplate()}
       </div>
     </div>
